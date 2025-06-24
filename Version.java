a511715b25ca65b95991a52a3855a8a5f06b0bbe
Jordy07 <jordyjoaquin07@gmail.com>
2025-06-24 09:26:16 -0600

    Primer Cambio Realizado

---
+++ Version.java
@@ -0,0 +1,17 @@
+public class Version{
+
+public static void main(String[] args) {
+    String nombre = "Jordy";
+
+    int a = 5; 
+
+    int b = 3; 
+
+    int suma = a + b; 
+
+    System.out.println("Hola, " + nombre +"!");
+    System.out.println("La suma de " + a + " y " + b +  "es: " + suma);
+}
+
+
+}
