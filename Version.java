0ded8bd313cb7a1f641579cb91de0a548db39eb9
Jordy-beep <jordyjoaquin07@gmail.com>
2025-06-24 10:26:58 -0600

    Tercer cambio realizado

--- Version.java
+++ Version.java
@@ -9,8 +9,14 @@ public static void main(String[] args) {
 
     int suma = a + b; 
 
+  int mult = a * b;
+
+double div =  a / b; 
+
     System.out.println("Hola, " + nombre +"!");
     System.out.println("La suma de " + a + " y " + b +  "es: " + suma);
+    System.out.println("La multiplicacion es: " + mult);
+    System.out.println("Divison: " + div);
 }
 
 
